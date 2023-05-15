bb7c1e9313b570669e59e02a4e3c6b43c3c43085
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-05-15 15:39:25 +0200

    netlify deploy

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Negotiator from "negotiator";
 
-import { i18n } from "./i18n-config";
+import { i18n } from "../i18n-config";
 
 function getLocale(request: NextRequest) {
   const negotiatorHeaders: Record<string, string> = {};

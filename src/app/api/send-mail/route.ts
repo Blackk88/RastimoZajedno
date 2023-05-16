d7880fc88039831d275356587a64da66ad2a90d0
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-05-16 23:22:42 +0200

    add fallback for subject field

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request, res: Response) {
     },
     replyTo: data.email,
     to: process.env.EMAIL_RECIPIENT,
-    subject: data.subject,
+    subject: data.subject ?? "No Subject",
     html: `
     <i>Message from Rastimo Zajedno Website</i>
     <h4>Message from: ${data.name} Email: ${data.email}</h4>
